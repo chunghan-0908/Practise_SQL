fceed62611d18e88ee1c3b445d8e4c10d94be107
chunghan-0908 <133232193+chunghan-0908@users.noreply.github.com>
2024-03-08 10:58:18 +0700

    Update Practice 5.sql

--- Practice 5.sql
+++ Practice 5.sql
@@ -13,3 +13,21 @@ LEFT JOIN texts t
 WHERE t.signup_action = 'Confirmed';
 
 --EX3
+SELECT 
+    age_bucket, 
+    ROUND(100.0* SUM(CASE WHEN "activity_type" = 'send' THEN act.time_spent ELSE NULL END) / SUM(CASE WHEN "activity_type" IN ('send','open') THEN "time_spent" ELSE NULL END), 2) AS send_perc,
+    ROUND(100.0* SUM(CASE WHEN "activity_type" = 'open' THEN act.time_spent ELSE NULL END) / SUM(CASE WHEN "activity_type" IN ('send', 'open') THEN "time_spent" ELSE NULL END), 2) AS open_perc
+FROM 
+    activities act 
+INNER JOIN 
+    age_breakdown age_ ON act.user_id = age_.user_id
+GROUP BY 
+    age_bucket;
+
+
+--EX4?
+SELECT customer_id,
+COUNT(DISTINCT product_category) AS product_
+FROM customer_contracts cus
+INNER JOIN products pro ON cus.product_id = pro.product_id
+GROUP BY customer_id;
